2ebf63cdca656c64d5cc31ee74e5d84050ea08e7
Lautaro Ibarrondo <ibarrondolautaro379@gmail.com>
2026-07-17 12:15:49 -0300

    Pre Entrega 1 JavaScript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nombre = prompt("Ingrese su nombre");
+const edad = parseInt(prompt("Ingrese su edad"));
+const ciudad = prompt("¿De qué ciudad sos?");
+
+let edadEnCinco = edad + 5;
+
+let mensaje = "Hola" +
+            nombre +
+            ".vivís en" +
+            ciudad +
+            "y dentro de 5 años vas a tener" +
+            edadEnCinco +
+            "años.";
+
+console.log(mensaje);
+alert(mensaje);
